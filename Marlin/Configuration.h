01b91d6f5ab0829678cabd90bbeed0ba4faeee8a
Jiang Sheng <doskoi.panda@gmail.com>
2018-07-19 19:34:38 +0800

    Fix y probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -758,7 +758,7 @@
  *    (0,0)
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 40  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 8.5  // Y offset: -front +behind [the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 8  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
